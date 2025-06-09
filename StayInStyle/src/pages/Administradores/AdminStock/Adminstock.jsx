29d57dfc4517938d69da9723c552759b0c5f4065
Dilandak <dilandakrg@gmail.com>
2025-06-09 15:22:01 -0500

    Hago commitukkk

--- StayInStyle/src/pages/Administradores/AdminStock/Adminstock.jsx
+++ StayInStyle/src/pages/Administradores/AdminStock/Adminstock.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./adminstock.css";
+import "./Adminstock.css";
 
 const AdminStock = () => {
   const [inventory, setInventory] = useState([]);
